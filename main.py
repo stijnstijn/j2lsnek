a5e6ac9a4167e5bd4a74b5224010c81d374820b1
Stijn <git@stijnpeeters.nl>
2017-03-09 00:27:36 +0100

    Request data sync from remote list servers on startup

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class listserver():
         if remotes:
             self.remotes = [remote["address"] for remote in remotes]
 
-        self.broadcast({"action": "hello"})  # let other list servers know we're live
+        self.broadcast({"action": "request"})  # let other list servers know we're live and ask them for the latest
         self.listen_to([10053, 10054, 10055, 10056, 10057, 10058, 10059])
 
     def listen_to(self, ports):
