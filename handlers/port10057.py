ea7551aa0b29c611c3c74dc3877e9235004de23f
stijnstijn <github@ident.stijnpeeters.nl>
2017-04-18 15:44:42 +0100

    Remove extra newline in 10057 response

--- handlers/port10057.py
+++ handlers/port10057.py
@@ -27,7 +27,6 @@ class ascii_handler(port_handler):
             list += str(int(time.time()) - int(server['created'])) + ' '            # uptime in seconds
             list += '[' + str(server['players']) + '/' + str(server['max']) + '] '  # [players/max]
             list += server['name']                                                  # server name
-            list += "\r\n"
 
         self.msg(list+"\r\n")
         self.end()
