eea52b694eb45cacbdefd207a672234813433524
stijnstijn <github@ident.stijnpeeters.nl>
2017-04-19 15:38:52 +0100

    Update bridge to new API

--- bridge.py
+++ bridge.py
@@ -15,6 +15,7 @@ while True:
         buffer += add
 
     servers = buffer.split("\n")
+    payload = []
     for server in servers:
         try:
             bits = server.split(" ")
@@ -32,23 +33,24 @@ while True:
             players = int(bits[1][1:-1].split("/")[0])
             max = int(bits[1][1:-1].split("/")[1])
             name = " ".join(bits[2:]).strip()
+            payload.append({"id": id, "ip": ip, "port": port, "created": created, "lifesign": int(time.time()), "private": private, "remote": 1, "origin": "bridge", "version": version, "mode": mode, "players": players, "max": max, "name": name})
         except ValueError:
             continue
 
-        payload = {"action": "server", "data": {"id": id, "ip": ip, "port": port, "created": created, "lifesign": int(time.time()), "private": private, "remote": 1, "origin": "bridge", "version": version, "mode": mode, "players": players, "max": max, "name": name}}
+    payload = {"action": "server", "data": servers, "origin": "bridge"}
 
-        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connection.settimeout(4)
-        connection.connect(("localhost", 10056))
-        connection.sendall(json.dumps(payload).encode("ascii"))
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.settimeout(4)
+    connection.connect(("localhost", 10056))
+    connection.sendall(json.dumps(payload).encode("ascii"))
 
-        try:
-            data = connection.recv(1024)
-            print("Synced server %s" % name)
-        except socket.timeout:
-            print("API call timed out")
+    try:
+        data = connection.recv(1024)
+        print("Synced server %s" % name)
+    except socket.timeout:
+        print("API call timed out")
 
-        connection.close()
+    connection.close()
 
     listserver.close()
 
