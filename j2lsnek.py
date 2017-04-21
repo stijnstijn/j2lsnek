2a930a22c0d42af59acc3d5fe4190fa666951643
stijnstijn <github@ident.stijnpeeters.nl>
2017-04-21 19:06:37 +0100

    Fix bug when restarting server twice in a row

--- j2lsnek.py
+++ j2lsnek.py
@@ -97,7 +97,7 @@ class listserver():
 
         # restart script if that mode was chosen
         if self.reboot_mode == "reboot":
-            time.sleep(1.0)
+            interpreter = sys.executable.split("/")[-1]
             os.execvp(sys.executable, ["j2lsnek"] + sys.argv)
 
     def listen_to(self, ports):
