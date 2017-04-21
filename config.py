5ec8454c2ec63146461ee3525bcfb90e8cfe7384
Stijn <git@stijnpeeters.nl>
2017-04-21 17:30:06 +0200

    ssl config in separate file

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ TIMEOUT = 40  # time until a server is delisted
 # ssl chain (for the server), certificates and keys that are used to authenticate remote admin interfaces - these
 # can be left empty and the list server will still work, port 10059 will just be unavailable if they're empty
 # or invalid
-CERTFILE = ""
-CERTCHAIN = ""
-CERTKEY = ""
-CLIENTCERT = ""
-CLIENTKEY = ""
+CERTFILE = "/srv/tcp/list/ssl/intermediate/certs/listserver_serverside.cert.pem"
+CERTCHAIN = "/srv/tcp/list/ssl/intermediate/certs/ca-chain.cert.pem"
+CERTKEY = "/srv/tcp/lists/ssl/intermediate/private/list.jazz2online.com.key.pem"
+CLIENTCERT = "/srv/tcp/list/ssl/intermediate/certs/listserver_clientside.pem"
+CLIENTKEY = "/srv/tcp/lists/ssl/intermediate/private/list/listserver_clientside.key.pem"
 
 # the following three values are for throttling/rate limiting
 # TICKSMAX is the max amount of ticks per IP - when reached the connection will be refused
