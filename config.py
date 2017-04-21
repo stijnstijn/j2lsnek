91f0eb9042107e91429261d465bf355f7b1a510e
Stijn <git@stijnpeeters.nl>
2017-04-21 17:33:12 +0200

    allow nonexistant sslconfig.py

--- config.py
+++ config.py
@@ -10,11 +10,19 @@ TIMEOUT = 40  # time until a server is delisted
 # ssl chain (for the server), certificates and keys that are used to authenticate remote admin interfaces - these
 # can be left empty and the list server will still work, port 10059 will just be unavailable if they're empty
 # or invalid
-CERTFILE = "/srv/tcp/list/ssl/intermediate/certs/listserver_serverside.cert.pem"
-CERTCHAIN = "/srv/tcp/list/ssl/intermediate/certs/ca-chain.cert.pem"
-CERTKEY = "/srv/tcp/lists/ssl/intermediate/private/list.jazz2online.com.key.pem"
-CLIENTCERT = "/srv/tcp/list/ssl/intermediate/certs/listserver_clientside.pem"
-CLIENTKEY = "/srv/tcp/lists/ssl/intermediate/private/list/listserver_clientside.key.pem"
+try:
+    import sslconfig
+    CERTFILE = sslconfig.CERTFILE
+    CERTCHAIN = sslconfig.CERTCHAIN
+    CERTKEY = sslconfig.CERTKEY
+    CLIENTCERT = sslconfig.CLIENTCERT
+    CLIENTKEY = sslconfig.CLIENTKEY
+except ImportError:
+    CERTFILE = ""
+    CERTCHAIN = ""
+    CERTKEY = ""
+    CLIENTCERT = ""
+    CLIENTKEY = ""
 
 # the following three values are for throttling/rate limiting
 # TICKSMAX is the max amount of ticks per IP - when reached the connection will be refused
