60f3b4d0ff4420774325db84fe4d1291d5aa5e56
stijnstijn <github@ident.stijnpeeters.nl>
2017-04-21 20:03:32 +0100

    Right, it's strip(), not trim()

--- helpers/jj2.py
+++ helpers/jj2.py
@@ -50,7 +50,7 @@ class jj2server():
 
         if item == "name":
             value = re.sub(r'[^\x00-\x7f]', r' ', value)  # no funny business with crazy characters
-            value = re.sub(r"[ ]+", r" ", value).trim()
+            value = re.sub(r"[ ]+", r" ", value).strip()
 
         if item == "max" or item == "players":
             if value > config.MAXPLAYERS:
