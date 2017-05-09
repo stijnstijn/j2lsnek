a5af805b80992b621d836db37fa237c88b0d7f3a
stijnstijn <github@ident.stijnpeeters.nl>
2017-05-09 13:48:16 +0100

    Allow local config overriding default config

--- config.py
+++ config.py
@@ -1,35 +1,8 @@
-"""
-Not a lot to configure
-"""
-VERSION = "03"
-DATABASE = "servers.db"
-MICROSLEEP = 0.2
-MAXPLAYERS = 32
-TIMEOUT = 40  # time until a server is delisted
-MAXSERVERS = 2  # max servers per IP
+# this is just a dummy file that imports the local configuration file if it exists, or the defaults if it doesn't
+# this way other files can still import "config" and always get the right values
 
-# ssl chain (for the server), certificates and keys that are used to authenticate remote admin interfaces - these
-# can be left empty and the list server will still work, port 10059 will just be unavailable if they're empty
-# or invalid
+from defaultconfig import *
 try:
-    import sslconfig
-    CERTFILE = sslconfig.CERTFILE
-    CERTCHAIN = sslconfig.CERTCHAIN
-    CERTKEY = sslconfig.CERTKEY
-    CLIENTCERT = sslconfig.CLIENTCERT
-    CLIENTKEY = sslconfig.CLIENTKEY
+    from localconfig import *
 except ImportError:
-    CERTFILE = ""
-    CERTCHAIN = ""
-    CERTKEY = ""
-    CLIENTCERT = ""
-    CLIENTKEY = ""
-
-# the following three values are for throttling/rate limiting
-# TICKSMAX is the max amount of ticks per IP - when reached the connection will be refused
-# TICKSDECAY is the rate per second at which ticks decay, e.g. for rate 1, every second one tick is "forgotten"
-# if TICKSMAX and TICKSDECAY are equal, one connection per second can be made
-# TICKSMAXAGE is the amount of time after which an IP will be forgotten by the rate limiter, if it hasn't connected
-TICKSMAX = 10
-TICKSDECAY = 2
-TICKSMAXAGE = 86400
+    pass
---
+++ defaultconfig.py
@@ -0,0 +1,27 @@
+"""
+Not a lot to configure
+"""
+VERSION = "03"
+DATABASE = "servers.db"
+MICROSLEEP = 0.2
+MAXPLAYERS = 32
+TIMEOUT = 40  # time until a server is delisted
+MAXSERVERS = 2  # max servers per IP
+
+# ssl chain (for the server), certificates and keys that are used to authenticate remote admin interfaces - these
+# can be left empty and the list server will still work, port 10059 will just be unavailable if they're empty
+# or invalid
+CERTFILE = ""
+CERTCHAIN = ""
+CERTKEY = ""
+CLIENTCERT = ""
+CLIENTKEY = ""
+
+# the following three values are for throttling/rate limiting
+# TICKSMAX is the max amount of ticks per IP - when reached the connection will be refused
+# TICKSDECAY is the rate per second at which ticks decay, e.g. for rate 1, every second one tick is "forgotten"
+# if TICKSMAX and TICKSDECAY are equal, one connection per second can be made
+# TICKSMAXAGE is the amount of time after which an IP will be forgotten by the rate limiter, if it hasn't connected
+TICKSMAX = 10
+TICKSDECAY = 2
+TICKSMAXAGE = 86400
